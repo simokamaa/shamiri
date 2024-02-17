e38d675f76e4f9ee1949266cbb6399c7d36bc71a
Simon Kamau <simonkamau690@gmail.com>
2024-02-17 11:41:48 +0300

    ResidentCard

--- shamiri/app/page.tsx
+++ shamiri/app/page.tsx
@@ -1,10 +1,12 @@
 import { Flex, Text, Button } from '@radix-ui/themes';
+import ResidentCard from './components/ResidentCard'
 
 export default function MyApp() {
   return (
     <Flex direction="column" gap="2">
       <Text>Hello </Text>
       <Button>Let's go</Button>
+      <ResidentCard/>
     </Flex>
   );
 }
