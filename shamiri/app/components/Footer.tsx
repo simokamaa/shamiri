a816824b8ff61f6c739b9539890e67c9b5a83e25
Simon Kamau <simonkamau690@gmail.com>
2024-02-17 12:32:07 +0300

    footer implementation

---
+++ shamiri/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { MergedFooterStyles } from "../styles/components/Footer";
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <p>2024 copyright</p>
+    </div>
+  )
+}
+
+export default Footer
